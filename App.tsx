6b183f8e6c6b56dd25fb8256b041e690df625e1b
Mykola Pelekh <mpelekh@lohika.com>
2020-04-21 14:45:26 +0300

    Add first section title
    * city name
    * day name

--- App.tsx
+++ App.tsx
@@ -9,68 +9,78 @@
  */
 
 import React from 'react';
-import {SafeAreaView, ScrollView, View, Text, StatusBar} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {SafeAreaView, View, Text, StatusBar} from 'react-native';
 
 import {styles} from './styles';
-
-import ReduxPoc from './components/ReduxPoc';
-
-declare const global: {HermesInternal: null | {}};
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+      <SafeAreaView style={styles.safeAreaViewTop} />
+      <SafeAreaView style={styles.safeAreaViewBottom}>
+        <View style={styles.container}>
+          <View style={styles.sectionTitle}>
+            <View>
+              <Text style={styles.cityNameText}>San Francisco</Text>
             </View>
-          )}
-          <ReduxPoc />
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
+            <View style={styles.cityNameIconPosition}>
+              <Text style={styles.cityNameIconText}>=</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
-        </ScrollView>
+          <View style={styles.sectionTitle}>
+            <View>
+              <Text style={styles.dayNameText}>Tuesday</Text>
+            </View>
+          </View>
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
 export default App;
+
+// const App = () => {
+//   return (
+//     <>
+//       <StatusBar barStyle="dark-content" />
+//       <SafeAreaView>
+//         <ScrollView
+//           contentInsetAdjustmentBehavior="automatic"
+//           style={styles.scrollView}>
+//           {/* <Header /> */}
+//           <ReduxPoc />
+//           <View style={styles.body}>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Step One</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Edit <Text style={styles.highlight}>App.tsx</Text> to change
+//                 this screen and then come back to see your edits.
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>See Your Changes</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <ReloadInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Debug</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <DebugInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Learn More</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Read the docs to discover what to do next:
+//               </Text>
+//             </View>
+//             <LearnMoreLinks />
+//           </View>
+//         </ScrollView>
+//       </SafeAreaView>
+//     </>
+//   );
+// };
--- styles.ts
+++ styles.ts
@@ -1,41 +1,79 @@
 import {StyleSheet} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const COLORS = {
+  lightBlue: '#1da9ec',
+  white: '#ffffff',
+};
 
 export const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  safeAreaViewTop: {
+    flex: 0,
+    backgroundColor: COLORS.lightBlue,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  safeAreaViewBottom: {
+    flex: 1,
+    backgroundColor: COLORS.lightBlue,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightBlue,
   },
   sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+    flexDirection: 'row',
+    paddingLeft: 25,
+    paddingRight: 25,
   },
-  sectionDescription: {
-    marginTop: 8,
+  cityNameText: {
+    color: COLORS.white,
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  dayNameText: {
+    color: COLORS.white,
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
   },
-  highlight: {
-    fontWeight: '700',
+  cityNameIconPosition: {
+    marginLeft: 'auto',
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  cityNameIconText: {
+    color: COLORS.white,
+    fontSize: 32,
   },
+
+  // scrollView: {
+  //   backgroundColor: Colors.lighter,
+  // },
+  // engine: {
+  //   position: 'absolute',
+  //   right: 0,
+  // },
+  // body: {
+  //   backgroundColor: Colors.white,
+  // },
+  // sectionContainer: {
+  //   marginTop: 32,
+  //   paddingHorizontal: 24,
+  // },
+  // sectionTitle: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  //   color: Colors.black,
+  // },
+  // sectionDescription: {
+  //   marginTop: 8,
+  //   fontSize: 18,
+  //   fontWeight: '400',
+  //   color: Colors.dark,
+  // },
+  // highlight: {
+  //   fontWeight: '700',
+  // },
+  // footer: {
+  //   color: Colors.dark,
+  //   fontSize: 12,
+  //   fontWeight: '600',
+  //   padding: 4,
+  //   paddingRight: 12,
+  //   textAlign: 'right',
+  // },
 });
